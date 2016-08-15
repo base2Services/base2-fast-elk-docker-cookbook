ad836c842fb0c679c535dec4e21d09df90a4d5a5
daraghmartin <d.martin@base2services.com>
2016-08-16 08:44:52 +1000

    fix templates recipe

---
+++ recipes/setup_directories_and_files.rb
@@ -0,0 +1,17 @@
+%w{logstash elasticsearch}.each do | dir |
+  directory "/var/config/#{dir}" do
+    recursive true
+  end
+end
+
+%w{ logstash.conf }.each do
+  template "/var/config/logstash/#{template}" do
+    source "logstash/#{template}"
+  end
+end
+
+%w{elasticsearch.yml docker-entrypoint.sh}.each do | template |
+  template "/var/config/elasticsearch/#{template}" do
+    source "elasticsearch/#{template}"
+  end
+end
