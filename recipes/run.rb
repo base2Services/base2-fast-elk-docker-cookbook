f296683d4430720eab6750c32ab614464f16c1b0
daraghmartin <d.martin@base2services.com>
2016-08-23 06:21:56 +1000

    fix kibana password with - spell config right

--- recipes/run.rb
+++ recipes/run.rb
@@ -65,7 +65,7 @@ docker_container 'nginx' do
   tag 'latest'
   port [ '80:80' ]
   links [ 'kibana:kibana' ]
-  volumes [ "/var/config/nginx/nginx.conf:/etc/nginx/conf.d/default.conf", "/var/conf/nginx/htpasswd.users:/etc/nginx/htpasswd.users" ]
+  volumes [ "/var/config/nginx/nginx.conf:/etc/nginx/conf.d/default.conf", "/var/config/nginx/htpasswd.users:/etc/nginx/htpasswd.users" ]
   log_driver = "json-file"
   log_opts "max-size=1g" #log_opts ["max-size=1g", "max-file=2", "labels=label1,label2", "env=evn1,env2"]
   detach true
